18745fcf47f10c9ec2fc1e715ac3247f99c5d1a1
AlphaFox-trot <bertrandrew@gmail.com>
2019-02-14 15:05:41 -0600

    set up a simple website, first assighnment created

---
+++ scene/sketch.js
@@ -0,0 +1,14 @@
+// Interactive Scene
+// Andrew Bertrand
+// Feb 14, 2019
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  ellipse(mouseX, mouseY, 50, 50)
+  ellipse(pmouseX, pmouseY, 50, 50)
+}
+
