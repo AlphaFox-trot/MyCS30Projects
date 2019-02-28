1a36e23289b8617b17605915b2e1bd1a0ce0da3d
AlphaFox-trot <bertrandrew@gmail.com>
2019-02-28 15:19:57 -0600

    Updated the creeper

--- creeperController/sketch.js
+++ creeperController/sketch.js
@@ -1,24 +1,32 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Creeper
+// Andrew
+// Feb 28, 2019
 
-let x = windowWidth;
-let y = windowHeight;
-let location = [x, y];
+let x, y;
+let size;
+let movement;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(220);
+  x = windowWidth / 2;
+  y = windowHeight / 2;
+  size = 20;
 }
 
 function draw() {
-  for(let i; i < 10; i++){
-    fill(200, 20, 225);
-    ellipse(5, 5, x, y);
-    x = windowWidth +- 2.5;
-    y = windowHeight +- 2.5;
+  fill (200, 20, 225);
+  ellipse(x, y, size);
+  movement = random(0, 10);
+
+  if (movement <= 2){
+    x = x - 20;
+  }
+  else if (movement <= 4){
+    y = y - 20;
+  }
+
+  if (size >= 5){
+    size -= 0.5;
   }
 }
