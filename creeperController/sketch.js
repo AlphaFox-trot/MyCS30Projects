118733dc34aabb2417eef544ddf296998ef3763d
AlphaFox-trot <bertrandrew@gmail.com>
2019-03-01 15:20:02 -0600

    finished pipe maker, worked on creeper

--- creeperController/sketch.js
+++ creeperController/sketch.js
@@ -5,28 +5,53 @@
 let x, y;
 let size;
 let movement;
+let hive;
+let location;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(220);
   x = windowWidth / 2;
   y = windowHeight / 2;
-  size = 20;
+  size = 30;
+  hive = 0;
+  location = [x, y];
 }
 
 function draw() {
   fill (200, 20, 225);
+  if (movement >= 9){
+    fill (0, 100, 50);
+    ellipse(x, y, size + 5);
+  }
   ellipse(x, y, size);
   movement = random(0, 10);
 
-  if (movement <= 2){
-    x = x - 20;
+  if (movement <= 4){
+    x = x - size - 2;
   }
-  else if (movement <= 4){
-    y = y - 20;
+  else if (movement <= 9){
+    y = y - size - 2;
+  }
+  else if (movement >= 9){
+    fill (0, 100, 50);
+    ellipse(x, y, size + 5);
   }
 
   if (size >= 5){
-    size -= 0.5;
+    size -= 0.3;
   }
+  if (hive === 10){
+    fill(225, 0, 0);
+    ellipse(windowWidth / 2, windowHeight / 2, 60);
+  }
+  if (hive === 25){
+    fill (100, 0, 0);
+    ellipse(windowWidth / 2, windowHeight / 2, 59, 20);
+  }
+  if (hive === 50){
+    fill (150, 0, 50);
+    ellipse(windowWidth / 2, windowHeight / 2, 20, 20);
+  }
+  hive++;
 }
--- moveAround/sketch.js
+++ moveAround/sketch.js
@@ -1,6 +1,12 @@
 // Pipe Screen
 // Andrew Bertrand
 // Feb 15, 2019
+// use WASD to move around
+// q and e to change your size
+// use r g b y p and t to change your colour
+// use z and x to change the background
+// use c and v to change to white or black
+// use space to randomise your colour
 
 let x;
 let y;
@@ -15,39 +21,72 @@ function setup() {
   x = 200;
   y = 200;
   size = 50;
-  background(220);
+  background(225);
   direction = 1;
 }
 
 function draw() {
   fill(colour1, colour2, colour3);
-  ellipse(x, y, size, size);
+  ellipse(x, y, size);
   if  (keyIsPressed){
-    if (key === 'w'){
-      if (y >= 0 + (size/2)){
+    if (key === "w"){
+      if (y >= 0 + size/2){
         y = y - 5;
       }
     }
-    if (key === 'a'){
-      if (x >= 0 + (size/2)){
+    if (key === "a"){
+      if (x >= 0 + size/2){
         x = x - 5;
       }
     }
-    if (key === 's'){
-      if (y <= windowHeight - (size/2)){
+    if (key === "s"){
+      if (y <= windowHeight - size/2){
         y = y + 5;
       }
     }
-    if (key === 'd'){
-      if (x <= windowWidth - (size/2)){
+    if (key === "d"){
+      if (x <= windowWidth - size/2){
         x = x + 5;
       }
     }
-    if (key === 'q'){
+    if (key === "q"){
       size--;
     }
-    if (key === 'e'){
+    if (key === "e"){
       size++;
+    }
+    if (key === "r"){
+      fill(225, 0, 0);
+    }
+    if (key === "g"){
+      fill(0, 225, 0);
+    }
+    if (key === "b"){
+      fill(0, 0, 225);
+    }
+    if (key === "p"){
+      fill(225, 0, 225);
+    }
+    if (key === "t"){
+      fill(0, 225, 225);
+    }
+    if (key === "y"){
+      fill(225, 225, 0);
+    }
+    if (key === "z"){
+      fill(225, 225, 225);
+    }
+    if (key === "x"){
+      fill(0, 0, 0);
+    }
+    if (key === "c"){
+      background(0);
+    }
+    if (key === "v"){
+      background(300);
+    }
+    if (key === " "){
+      fill(random(0, 225), random(0, 225), random(0, 225));
     }
   }
 }
