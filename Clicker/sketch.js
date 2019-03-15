696d1087c2be763eac818f1a9c68c91f34788328
AlphaFox-trot <bertrandrew@gmail.com>
2019-03-15 15:19:52 -0600

    Started strange clicker

--- Clicker/sketch.js
+++ Clicker/sketch.js
@@ -1,41 +1,114 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Hax Simulator
+// Felix
+// March 15
 
 let colour, name;
-let event, level;
-let power;
+let level, area, menu, lives, cash, price;
+let timer;
+let listColours = ["red", "blue", "green", "yellow", "purple", "cyan"];
+let buttonX1, buttonX2, buttonY1, buttonY2;
+let startLevel, extraCash, extraLives;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  menu = "main";
+  buttonX1 = windowWidth/2;
+  buttonX2 = windowWidth/2;
+  buttonY1 = windowHeight/3;
+  buttonY2 = windowHeight/1.5;
+  startLevel = 1;
+  extraCash = 1;
+  extraLives = 2;
+  cash = 50;
+  price = 10;
 }
 
 function draw() {
   background(220);
+  showMenu();
+  if (mouseIsPressed && menu === "main" && mouseX >= buttonX1 - 125 && mouseX <= buttonX1 + 125 && mouseY >= buttonY1 - 50 && mouseY <= buttonY1 + 50){
+    menu = "play";
+  }
+  if (mouseIsPressed && menu === "main" && mouseX >= buttonX2 - 125 && mouseX <= buttonX2 + 125 && mouseY >= buttonY2 - 50 && mouseY <= buttonY2 + 50){
+    menu = "upgrades";
+    buttonY2 = windowHeight/1.2;
+  }
+  else if (mouseIsPressed && menu === "upgrades" && mouseX >= buttonX2 - 125 && mouseX <= buttonX2 + 125 && mouseY >= buttonY2 - 50 && mouseY <= buttonY2 + 50){
+    menu = "main";
+    buttonY2 = windowHeight/1.5;
+  }
 }
 
 function keyPressed() {
-  if (keyCode === LEFT_ARROW && colour === name && event === "normal") {
+  if (menu === "play" && keyCode === LEFT_ARROW && colour === name) {
     progress();
   } 
-  else if (keyCode === RIGHT_ARROW && colour !== name && event === "normal") {
+  else if (menu === "play" && keyCode === RIGHT_ARROW && colour !== name) {
     progress();
+  }
+  else{
+    lives--;
   }
 }
 
 function progress(){
   level ++;
-  event = random (0, 6);
-  if (event === 5){
-    power = 0;
+  colour = random (listColours);
+  name = colour;
+}
+
+function generateWords(){
+  if (colour === "red"){
+    fill(225, 0, 0);
+  }
+  else if (colour === "green"){
+    fill(0, 225, 0);
+  }
+  else if (colour === "blue"){
+    fill(0, 0, 225);
+  }
+  else if (colour === "yellow"){
+    fill(225, 225, 0);
+  }
+  else if (colour === "purple"){
+    fill(225, 0, 225);
+  }
+  else if (colour === "cyan"){
+    fill(0, 225, 225);
   }
 }
 
-function mousePressed(){
-  if(power >= 10){
-    power++;
+function showMenu(){
+  if (menu === "main"){
+    textSize(30);
+    fill(10, 200, 10);
+    rectMode(CENTER);
+    rect(buttonX1, buttonY1, 250, 100);
+    fill(0);
+    text("play", buttonX1, buttonY1);
+    fill(10, 200, 10);
+    rectMode(CENTER);
+    rect(buttonX2, buttonY2, 250, 100);
+    fill(0);
+    text("upgrades", buttonX2, buttonY2);
+  }
+  if (menu === "upgrades"){
+    fill(10, 200, 10);
+    rectMode(CENTER);
+    rect(buttonX2, buttonY2, 250, 100);
+    fill(0);
+    text("back", buttonX2, buttonY2);
+    text("push Q for a different starter level", 20, 50);
+    text("current starter level " + startLevel, 500, 50);
+    text("push W for more cash earnings", 20, 100);
+    text("current earnings " + extraCash, 500, 100);
+    text("push E for extra lives", 20, 150);
+    text("lives " + extraLives, 500, 150);
+    text("current cash " + cash, 50, 200);
+  }
+  if (menu === "play"){
+    generateWords();
+    text(name, windowWidth/2, windowHeight/2);
   }
 }
+
